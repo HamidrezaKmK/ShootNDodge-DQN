13c8ef29f8af534f981b1b2954e32ae48a86532e
HamidrezaKmK <hamidrezakamkari@gmail.com>
2022-01-02 03:30:20 +0330

    rewards changed

--- gym-shootndodge/setup.py
+++ gym-shootndodge/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gym_shootndodge',
-      version='0.0.991',
+      version='0.0.992',
       author='Hamidreza Kamkari',
       description='''
       A new atari game involving a shooter that can go sideways and shoot and multiple deadly astroid blocks comming at it!
